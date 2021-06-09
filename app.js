b64056ec06d32b8320d17aec90efd16a98ad02ed
Angel <andraco660@gmail.com>
2021-06-09 22:03:27 +0200

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const express = require('express')
- const cors = require("cors");
+//const cors = require("cors");
 
 const indexController = require('./controllers/indexController')
 const mainPageController = require('./controllers/mainPageController')
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 require('./modules/authenticator')
-app.use(cors())
+//app.use(cors())
 app.use(indexController)
 app.use(mainPageController)
 //app.use(userController)
