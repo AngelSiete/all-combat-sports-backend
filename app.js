b274b91d210299e7bc0b068f336ea3a821892c80
Angel <andraco660@gmail.com>
2021-06-12 00:04:07 +0200

    added auth & login structure, not functional yet

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict'
 
 const express = require('express')
-//const cors = require("cors");
+const cors = require("cors");
 
 const indexController = require('./controllers/indexController')
 const mainPageController = require('./controllers/mainPageController')
-//const userController = require('./controllers/usersControllers')
-//const authController = require('./controllers/authController')
+const userController = require('./controllers/usersControllers')
+const authController = require('./controllers/authController')
 const database = require('./modules/database')
 
 const app = express()
 
 app.use(express.json())
 require('./modules/authenticator')
-//app.use(cors())
+app.use(cors())
 app.use(indexController)
 app.use(mainPageController)
-//app.use(userController)
-//app.use(authController)
+app.use(userController)
+app.use(authController)
 
 database.connect()
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-//const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const config = require('../modules/config')
 
