fb651c9110de9f79223c2439ee65630d953bc903
Angel <andraco660@gmail.com>
2021-06-13 11:55:04 +0200

    changes 4 heroku

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 const express = require('express')
 const cors = require("cors");
-
 const indexController = require('./controllers/indexController')
 const mainPageController = require('./controllers/mainPageController')
 const userController = require('./controllers/usersControllers')
 const authController = require('./controllers/authController')
 const database = require('./modules/database')
+const bearerToken = require('express-bearer-token');
 
 const app = express()
 
 app.use(express.json())
 require('./modules/authenticator')
+app.use(bearerToken())
 app.use(cors())
 app.use(indexController)
 app.use(mainPageController)
--- modules/database.js
+++ modules/database.js
@@ -12,8 +12,8 @@ class Database {
     this.db = mongoose.connection;
 
     try {
-      await mongoose.connect(config.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-      console.log('Estamos conectados a la base de datos')
+            await mongoose.connect(proces.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+      console.log('Estamos conectados a MongoDB')
     }
     catch(error) {
       console.info('Error al conectar con la base de datos')
