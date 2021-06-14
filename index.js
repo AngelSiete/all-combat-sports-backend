16faa8a524072bcafe96eff13c0e431129a39a38
Angel <andraco660@gmail.com>
2021-06-14 12:27:42 +0200

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const config = require('./modules/config')
 const app = require('./app')
+//
+//
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.info(`Servidor en http://localhost:${PORT}`))
