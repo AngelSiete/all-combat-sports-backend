9f23edd8f1ed29a2e3c2a6381f8c9fcdc0092a93
Angel <andraco660@gmail.com>
2021-06-14 12:32:32 +0200

    heroku change

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const config = require('./modules/config')
 const app = require('./app')
 //
-//
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.info(`Servidor en http://localhost:${PORT}`))
